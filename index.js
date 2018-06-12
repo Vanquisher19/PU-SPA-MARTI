d6bfc153183866414503f0bd4df8018ff5075b2b
Vanquisher19 <marti.petrov19@gmail.com>
2018-06-12 13:09:36 +0300

    added function login

--- index.js
+++ index.js
@@ -26,7 +26,21 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
-
+function login() {
+	
+	var userEmail = document.getElementById("email_field").value;
+	var userPass = document.getElementById("password_field").value;
+	
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+	// Handle Errors here.
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	
+	window.alert("Error: " + errorMessage);
+	// ...
+});
+	
+}
 
 
 
